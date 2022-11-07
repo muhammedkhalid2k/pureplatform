4f6156c80a2611eef5d5b91ae01546a1f6f907df
muhammedkhalid2k <115889820+muhammedkhalid2k@users.noreply.github.com>
2022-11-07 22:08:18 +0300

    task

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,19 @@ class User < ApplicationRecord
     validates_length_of :username, :minimum => 3 , allow_black:false 
     validates :name, presence: true
     validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-    validates :username, uniqueness: { case_sensitive: false }
+    validates :identifier, presence: true
     has_one:profile
     has_one:car
     has_many:watches
     has_one:job
+
+    
+    before_validation :generate_id, on: :create 
+
+    private
+    def generate_id
+       self.identifier="iraq-#{Date.today.year}-#{SecureRandom.hex(5)}"
+        end
+
+    
 end
